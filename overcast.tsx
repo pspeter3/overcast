15cdefc21b458f68cb0a09a02b60d12d0b8a66cc
Phips Peter <pspeter333@gmail.com>
2017-04-05 18:36:17 -0700

    Add formatting for form inputs
    Relevant form inputs are now styled.

--- overcast.tsx
+++ overcast.tsx
@@ -1,1 +1,24 @@
-preact.render(<h1>Overcast</h1>, document.body)
+const content = (
+    <form>
+        <div class="cell">
+            <input type="text" name="name" placeholder="Sombra"/>
+            <label for="name">Name</label>
+        </div>
+        <div class="cell">
+            <input type="number" name="power" min="0" max="10" placeholder="9000"/>
+            <label for="power">Power</label>
+        </div>
+        <div class="cell">
+            <select name="type">
+                <option>Warrior</option>
+            </select>
+            <label for="type">Type</label>
+        </div>
+        <div class="cell">
+            <textarea name="notes" placeholder="Hack The Planet"/>
+            <label for="notes">Notes</label>
+        </div>
+    </form >
+)
+
+preact.render(content, document.body)
