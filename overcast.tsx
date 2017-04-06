a09a583452eaf9ee4123cdba0c0129a0881263b8
Phips Peter <pspeter333@gmail.com>
2017-04-06 15:05:20 -0700

    Fix lint issues

--- overcast.tsx
+++ overcast.tsx
@@ -1,22 +1,20 @@
 /**
  * UI
  */
-const AppBar = (): JSX.Element => {
-    return (
-        <header>
-            <a class="menu">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path d="M2 13.5h14V12H2v1.5zm0-4h14V8H2v1.5zM2 4v1.5h14V4H2z" />
-                </svg>
-            </a>
-            <span class="title">Overcast</span>
-        </header>
-    )
-}
+const appbar = (
+    <header>
+        <a class="menu">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                <path d="M2 13.5h14V12H2v1.5zm0-4h14V8H2v1.5zM2 4v1.5h14V4H2z" />
+            </svg>
+        </a>
+        <span class="title">Overcast</span>
+    </header>
+)
 
 /**
  * Init
  */
 window.addEventListener("load", () => {
-    preact.render(<AppBar />, document.body)
+    preact.render(appbar, document.body)
 })
-
