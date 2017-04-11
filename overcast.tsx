13514a7883e5d8bfbd0d106a1e6e2282fabedafb
Phips Peter <pspeter333@gmail.com>
2017-04-10 17:30:08 -0700

    Remove overflow menu

--- overcast.tsx
+++ overcast.tsx
@@ -24,11 +24,6 @@ const AppBar = (props: { updateReady?: boolean }): JSX.Element => {
         : null
     return (
         <header>
-            <a class="menu icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                    <path d="M2 13.5h14V12H2v1.5zm0-4h14V8H2v1.5zM2 4v1.5h14V4H2z" />
-                </svg>
-            </a>
             <span class="title">Overcast</span>
             {upgrade}
         </header>
